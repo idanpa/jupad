e03a6b4cb5c8545f4c0901098b99a11c4a3ccb81
Idan Pazi <idan.kp@gmail.com>
2023-09-01 11:23:17 +0300

    skip unchanged lines
    ignore write errors

--- pypad/__main__.py
+++ pypad/__main__.py
@@ -12,6 +12,7 @@ class NotepadFileHandler(PatternMatchingEventHandler):
         super().__init__(patterns=[file_path])
         self.observer = observer
         self.file_path = file_path
+        self.prev_lines = []
 
         sys.argv = [sys.argv[0]]
         config = Config()
@@ -24,10 +25,14 @@ class NotepadFileHandler(PatternMatchingEventHandler):
         self.ip = ipapp.shell
 
     def on_modified(self, event):
+        print('modified event')
         with open(self.file_path, 'r') as f:
             lines = f.read().splitlines()
         lines_done = []
-        while len(lines) > 0:
+        # skip unchanged lines
+        while len(lines) and len(self.prev_lines) and lines[0] == self.prev_lines.pop(0):
+            lines_done.append(lines.pop(0))
+        while len(lines):
             cell = [lines.pop(0)]
             while self.ip.check_complete('\n'.join(cell))[0] == 'incomplete' and len(lines) > 0:
                 cell.append(lines.pop(0))
@@ -37,17 +42,22 @@ class NotepadFileHandler(PatternMatchingEventHandler):
             res = '❌' if result.error_in_exec else result.result
             if res:
                 cell[-1] += f' #: {res}'
-                with open(self.file_path, 'w') as f:
-                    f.writelines('\n'.join(lines_done + cell + lines) + '\n')
-                while(not self.observer.event_queue.empty()):
-                    self.observer.event_queue.get(timeout=1)
+                try:
+                    with open(self.file_path, 'w') as f:
+                        f.writelines('\n'.join(lines_done + cell + lines) + '\n')
+                    while(not self.observer.event_queue.empty()):
+                        self.observer.event_queue.get(timeout=1)
+                except Exception as e:
+                    print(repr(e))
             lines_done += cell
+        # todo: but what if last write failed?
+        self.prev_lines = lines_done
 
 class NotepadObserver(Observer):
     def __init__(self, file_path, timeout=1):
         super().__init__(timeout=timeout)
-        handler = NotepadFileHandler(self, file_path)
-        self.schedule(handler, os.path.dirname(file_path))
+        self.handler = NotepadFileHandler(self, file_path)
+        self.schedule(self.handler, os.path.dirname(file_path))
 
 def main():
     parser = argparse.ArgumentParser()
