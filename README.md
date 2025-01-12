77e40c7e26950033afdae269e03d2e58b21f52e3
Idan Pazi <idan.kp@gmail.com>
2025-01-12 08:28:17 +0200

    icon readme demo

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# PyPad - Python Notepad
 
-Inspired by notepad calculators (e.g. [Soulver](https://soulver.app/), [Numbr](https://numbr.dev/))
+<h1 align="center">
+  <img src="pypad/resources/icon.svg" height="40" style="vertical-align:bottom"> PyPad - Python Notepad
+</h1>
+
+
+Python as a calculator, notepad style.  
+
+Inspired by notepad calculators ([Soulver](https://soulver.app/), [Numi](https://numi.app/), [Numbr](https://numbr.dev/)), reactive jupyter notebooks ([marimo](https://github.com/marimo-team/marimo), [ipyflow](https://github.com/ipyflow)) and similar projects ([Hydrogen](https://github.com/nteract/hydrogen)).  
+
+Based on [qtconsole](https://github.com/jupyter/qtconsole).
 
 ## Usage
 ```
--- pypad/__main__.py
+++ pypad/__main__.py
@@ -1,8 +1,18 @@
+import os
 import sys
 import argparse
 
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from pypad import MainWindow
+
+if os.name == 'nt':
+    # for taskbar icon
+    try:
+        from ctypes import windll
+        windll.shell32.SetCurrentProcessExplicitAppUserModelID(u'pypad.pypad')
+    except AttributeError:
+        pass
 
 def main():
     parser = argparse.ArgumentParser()
@@ -10,6 +20,12 @@ def main():
     args = parser.parse_args()
 
     app = QApplication([])
+
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    icon_path = os.path.join(base_path, 'resources', 'icon.svg')
+    app.icon = QIcon(icon_path)
+    app.setWindowIcon(app.icon)
+
     main_window = MainWindow(debug=args.debug)
     sys.exit(app.exec())
 
