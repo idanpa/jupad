129bc926a4edde08771e9faf59e06a3dc4d0278d
Idan Pazi <idan.kp@gmail.com>
2025-02-24 10:54:50 +0200

    fix recalculate columns
    some libraries, such as tqdm, do not use shutil to get terminal size

--- jupad/__init__.py
+++ jupad/__init__.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 import traceback
+import subprocess
 import logging
 from base64 import b64decode
 from contextlib import contextmanager
@@ -680,6 +681,7 @@ class JupadTextEdit(QTextEdit, BaseFrontendMixin):
             # update splash
             self.set_splash(True)
 
+        self.recalculate_columns()
         self.execute(0)
 
         self.setUndoRedoEnabled(True)
@@ -997,9 +999,13 @@ class JupadTextEdit(QTextEdit, BaseFrontendMixin):
         padding = 10
         columns = int((width-padding) // self.char_width)
         lines = int((self.viewport().height()-padding) // self.char_height)
+        if os.name == 'nt':
+            subprocess.Popen(f'mode {columns},{lines}', shell=True)
+        elif os.name == 'posix':
+            subprocess.Popen(['stty', 'rows' ,f'{lines}', 'cols', f'{columns}'])
+        # above is not enough, on linux shutil.get_terminal_size() looks at a wrapper of stdout and fails, on windows we are in gui mode, no terminal
         self.kernel_client.execute(f'import os\nos.environ["COLUMNS"] = "{columns}"\nos.environ["LINES"] = "{lines}"', silent=True, stop_on_error=False)
-
-        # new pictures would use the new width
+        # new output would use the new width
         self.execute(0)
 
     def paintEvent(self, event):
